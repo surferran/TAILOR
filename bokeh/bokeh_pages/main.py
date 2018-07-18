5cd356ad9d87b6bc5a0689b53da80b5fef2af234
surferran <surferran@yahoo.com>
2018-07-18 23:28:42 +0300

    working version

--- bokeh/bokeh_pages/main.py
+++ bokeh/bokeh_pages/main.py
@@ -14,7 +14,7 @@ import pandas as pd
 from bokeh.layouts import row, widgetbox, layout, column
 from bokeh.models import ColumnDataSource, CustomJS
 from bokeh.models.widgets import Slider, Button, DataTable, TableColumn, NumberFormatter, CheckboxGroup, RadioGroup, Toggle
-from bokeh.io import curdoc, output_file
+from bokeh.io import curdoc
 
 from bokeh.server.server import Server
 from bokeh.application import Application
@@ -22,20 +22,8 @@ from bokeh.application.handlers.function import FunctionHandler
 from bokeh.plotting import figure
 from bokeh.plotting import ColumnDataSource as plt_ColumnDataSource 
 
-def event_chart_example(doc):
-    factors = ["a","b","c,","d"]
-    x = [24.3, -22.3, -25, 6]
-    
-    LineColor=["green" if a>=0 else "red" for a in x]
-    
-    dot = figure(title="exapmple", y_range = factors, x_range = [-30,30])
-    
-    dot.segment(0, factors, x, factors, line_width=2, line_color=LineColor)
-    dot.circle(x, factors, size=15, fill_color="orange", line_width=3,line_color=LineColor)
-    
-# ?   output_file("sec_indeces_output.html", title="events graph example")
-    
-    doc.add_root(dot)
+#import bokeh
+#print (bokeh.__version__)
 
 def make_document(doc):
     doc.title = "Hello, world!"
@@ -109,7 +97,7 @@ def make_document(doc):
         print("cradio case")
         print(checked_option_ndx)
 #    toggle   = Toggle(label='Some on/off', button_type='success')
-    toggle   = Button(label='change table by source', button_type='success')#, callback=callback)
+    toggle   = Button(label='change table by source', button_type='success', callback=callback)
 #    toggleLayout = layout([toggle])
     
     checkbox = CheckboxGroup(labels=['foo', 'bar', 'baz'])
@@ -117,22 +105,44 @@ def make_document(doc):
 #    toggle.on_click(isToggleActive)
     checkbox.on_click(on_chkbx_clicked)
     radio.on_click(on_chkbx_clicked)
-    
-    fig3 = figure(title='bars from table', width=500, height=400)
-    fig3.bar(x='years_experience', y='salary', source=source)
-#             
-
-
+#    
+#    def set_vbar():
+#        fruits = ['Apples', 'Pears', 'Nectarines', 'Plums', 'Grapes', 'Strawberries']
+#        
+#        fig3 = figure(x_range=fruits, plot_height=250, title="Fruit Counts", toolbar_location=None, tools="")
+#        
+#        fig3.vbar(x=fruits, top=[5, 3, 4, 2, 4, 6], width=0.9)
+#        
+#        fig3.xgrid.grid_line_color = None
+#        fig3.y_range.start = 0
+#    
+#        return fig3 
+#    
+    def event_chart_example():
+        factors = ["a","b","c,","d"]
+        x = [24.3, -22.3, -25, 6]
+        
+        LineColor=["green" if a>=0 else "red" for a in x]
+        
+        dots = figure(title="exapmple", y_range = factors, x_range = [-30,30])
+        
+        dots.segment(0, factors, x, factors, line_width=2, line_color=LineColor)
+        dots.circle(x, factors, size=15, fill_color="orange", line_width=3,line_color=LineColor)
+        
+        return dots
 
 #    doc.add_root(fig1)
 #    doc.add_root(fig2)
     phase1 = column(table, slider)
     phase2 = row(phase1, fig2)
-    phase3 = column(toggle , checkbox, radio)
+#    phase3 = column(phase2, event_chart_example())#, set_vbar())
+#    phase3 = column(toggle , checkbox, radio)
     
     doc.add_root(phase2)
 #    doc.add_root(phase3)
 #    doc.add_root(toggleLayout)
+    
+#    doc.add_root(event_chart_example())
     
     callback.args['a']=slider
     callback.args['b']=toggle
@@ -198,26 +208,30 @@ if __name__=='__main__':
     
     apps1 = {'/': Application(FunctionHandler(make_document))}
     apps2 = {'/': Application(FunctionHandler(make_page_flow))}
-    apps2 = {'/': Application(FunctionHandler(event_chart_example))}
+#    apps3 = {'/': Application(FunctionHandler(event_chart_example))}
     
-    server1 = Server(apps1, port=5008)
+    server1 = Server(apps1, port=5007)
     server1.start()
-#    server2 = Server(apps2, port=5007)
-#    server2.start()
-    server2 = Server(apps2, port=5009)
+    server2 = Server(apps2, port=5008)
     server2.start()
+#    server3 = Server(apps3, port=5009)
+#    server3.start()
 #    make_document()
     
     server1.show('/')
-#    print(server2.port)
+#    print(server1.port)
     server2.show('/')
+#    server3.show('/')
     
 #    # then http://localhost:5008/
     
     section=''
+#    tmp=input("Press Enter to continue...")
+#    section='special stop'
     if section=='special stop':
         server1.stop()
         server2.stop()
+#        server3.stop()
     
 else:
     print("not main caller")
