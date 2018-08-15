e8a154751471e394aa49fb6c382bc1df639951b0
ran <sern@yahoo.com>
2018-08-15 07:47:52 +0300

    up

--- closed_examples/_external_help_collections/Untitled4.py
+++ closed_examples/_external_help_collections/Untitled4.py
@@ -50,3 +50,43 @@ plt.show()
 # %add line
 # line=[point;point+normr(normal)]
 # hold on;plot3(line(:,1),line(:,2),line(:,3),'LineWidth',5)
+
+####################
+
+mport numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+p1    = np.array([-1489., -4913.,  4345.])
+p2    = np.array([ 2633., -3268.,  5249.])
+pcalc = np.array([-3210., -4390.,  3930.])
+
+def normit(v):
+    return v / np.sqrt((v**2).sum())
+
+n1, n2, ncalc = [normit(p) for p in [p1, p2, pcalc]]
+
+new_zaxis  = normit(np.cross(n1, n2))
+new_xaxis  = n1
+zero       = np.zeros(3) 
+
+fig = plt.figure(figsize=[10, 8])
+ax  = fig.add_subplot(1, 1, 1, projection='3d')
+
+x, y, z = zip(zero, new_xaxis)
+plt.plot(x, y, z, '-k', linewidth=3)
+
+x, y, z = zip(zero, new_zaxis)
+plt.plot(x, y, z, '--k', linewidth=3)
+
+x, y, z = zip(zero, n2)
+plt.plot(x, y, z, '-r', linewidth=1)
+
+x, y, z = zip(zero, ncalc)
+plt.plot(x, y, z, '--g', linewidth=1)
+
+ax.set_xlim(-1, 1)
+ax.set_ylim(-1, 1)
+ax.set_zlim(-1, 1)
+
+plt.show()
